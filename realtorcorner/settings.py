f269a8d4375bd6378960699a88090c638fc37275
JC-April <jimkronicles@gmail.com>
2024-12-13 18:05:45 +0100

    added .env and git ignore files

--- realtorcorner/settings.py
+++ realtorcorner/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import os
 import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY",'django-insecure-yo6$7#!rrxm*anwndhu*k1(_r5dmy&8=h0$$d8nw+qm^p(yhmz')
+# SECRET_KEY = os.environ.get("SECRET_KEY",'')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
