ca6a77fef91c1674d1961e2c32413816ca8c5815
JC-April <jimkronicles@gmail.com>
2024-12-14 00:06:05 +0100

    created create_superuser file

--- base/apps.py
+++ base/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
-from django.contrib.auth import get_user_model
-import os
+# from django.contrib.auth import get_user_model
+# import os
 
 
 class BaseConfig(AppConfig):
@@ -9,18 +9,18 @@ class BaseConfig(AppConfig):
 
 # to create superuser on app startup if it doesn't already exist
 
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+# class UsersConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'users'
 
-    def ready(self):
-        User = get_user_model()
-        if not User.objects.filter(is_superuser=True).exists():
-            User.objects.create_superuser(
-                username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
-                email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
-                password=os.getenv('DJANGO_SUPERUSER_PASSWORD')
-            )
-            print("Superuser created successfully")
+#     def ready(self):
+#         User = get_user_model()
+#         if not User.objects.filter(is_superuser=True).exists():
+#             User.objects.create_superuser(
+#                 username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+#                 email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+#                 password=os.getenv('DJANGO_SUPERUSER_PASSWORD')
+#             )
+#             print("Superuser created successfully")
 
 
---
+++ create_superuser.py
@@ -0,0 +1,26 @@
+import os
+# from django.core.management import execute_from_command_line
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+def create_superuser():
+    username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+    email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+    password=os.getenv('DJANGO_SUPERUSER_PASSWORD')
+
+    if username and email and password:
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+                email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+                password=os.getenv('DJANGO_SUPERUSER_PASSWORD')
+            )
+            print(f"superuser {username} created")
+        else:
+            print(f"superuser {username} already exists")
+    else:
+        print(f"environment variables not properly set for superuser creation")
+
+if __name__ == '__main__':
+    create_superuser()
