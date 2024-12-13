a82fa63202b26599a1ff0cf65c33c757b53f9c6d
JC-April <jimkronicles@gmail.com>
2024-12-13 19:46:09 +0100

    added env variables for superuser creation

--- base/apps.py
+++ base/apps.py
@@ -1,6 +1,26 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+import os
 
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
+
+# to create superuser on app startup if it doesn't already exist
+
+class UsersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'users'
+
+    def ready(self):
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username=os.getenv('DJANGO_SUPERUSER_USERNAME'),
+                email=os.getenv('DJANGO_SUPERUSER_EMAIL'),
+                password=os.getenv('DJANGO_SUPERUSER_PASSWORD')
+            )
+            print("Superuser created successfully")
+
+
