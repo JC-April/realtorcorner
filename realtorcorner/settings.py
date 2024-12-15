dbaa8132483649680bd6c21cd8671e40141a761c
JC-April <jimkronicles@gmail.com>
2024-12-15 22:08:46 +0100

    added front end, modified urls for static urls rendering

--- realtorcorner/settings.py
+++ realtorcorner/settings.py
@@ -186,7 +186,10 @@ STORAGES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "https://myrealtorcorner.onrender.com"
+]
 
 
 
